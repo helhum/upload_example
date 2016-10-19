aeefe6e18bf62d5bea94dbf638db150be382dfc5
Helmut Hummel <info@helhum.io>
2016-10-19 09:08:50 +0200

    [BUGFIX] Make PHP 7 compatible

--- Classes/Domain/Model/FileReference.php
+++ Classes/Domain/Model/FileReference.php
@@ -38,9 +38,16 @@ class FileReference extends \TYPO3\CMS\Extbase\Domain\Model\FileReference {
 	protected $originalFileIdentifier;
 
 	/**
+	 * @param \TYPO3\CMS\Core\Resource\ResourceInterface $originalResource
+	 */
+	public function setOriginalResource(\TYPO3\CMS\Core\Resource\ResourceInterface $originalResource) {
+		$this->setFileReference($originalResource);
+	}
+
+	/**
 	 * @param \TYPO3\CMS\Core\Resource\FileReference $originalResource
 	 */
-	public function setOriginalResource(\TYPO3\CMS\Core\Resource\FileReference $originalResource) {
+	private function setFileReference(\TYPO3\CMS\Core\Resource\FileReference $originalResource) {
 		$this->originalResource = $originalResource;
 		$this->originalFileIdentifier = (int)$originalResource->getOriginalFile()->getUid();
 	}
